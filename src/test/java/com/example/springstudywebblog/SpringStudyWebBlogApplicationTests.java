d3a824d6143d9f2b49e7001aed9eac0a79043dec
дом <amir/shakirov.2015@mail.ru>
2023-06-17 19:00:13 +0400

    Initial commit

---
+++ src/test/java/com/example/springstudywebblog/SpringStudyWebBlogApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.springstudywebblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyWebBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
